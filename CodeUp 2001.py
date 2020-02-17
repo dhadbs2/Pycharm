73b878871af5d5de840529b5a4eabe1975c826c1
dhadbs2 <dhadbs2@naver.com>
2020-02-17 21:04:25 +0900

    코드업 2001번 문제 풀이(수정)

--- CodeUp 2001.py
+++ CodeUp 2001.py
@@ -1,15 +1,9 @@
-a = int(input())
-b = int(input())
-c = int(input())
-x = int(input())
-y = int(input())
 list1 = []
 list2 = []
-list1.append(a)
-list1.append(b)
-list1.append(c)
-list2.append(x)
-list2.append(y)
+for i in range(3):
+    list1.append(int(input()))
+for i in range(2):
+    list2.append(int(input()))
 list1.sort()
 list2.sort()
 print(round((list1[0]+list2[0])*(1.1), 1))
